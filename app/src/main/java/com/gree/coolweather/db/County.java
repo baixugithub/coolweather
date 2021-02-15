647e9c057f88555fb67c264f4c47ba5478c7b7db
unknown <A20207@internet.com>
2021-02-15 16:34:31 +0800

    加入创建的数据库和表的各项配置。

---
+++ app/src/main/java/com/gree/coolweather/db/County.java
@@ -0,0 +1,38 @@
+package com.gree.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;             //每个实体类都应该有的字段
+    private String countyName;  //记录县的名字
+    private String weatherId;   //记录县所对应的天气id
+    private int cityId;         //记录当前市所属的id值
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countyName){
+        this.countyName = countyName;
+    }
+    public String getWeatherId(){
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+
+
+}
